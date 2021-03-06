2f11aa36901de2dce49f31f40ac961c446eb57f8
Alexandra Andritoi <alexandraandritoi1@gmail.com>
2021-03-06 17:44:46 +0200

    Added Migrations

---
+++ TheOfficeFurnitureWarehouse.Data/Migrations/20210306153614_initialcreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheOfficeFurnitureWarehouse.Data.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    ProductType = table.Column<int>(type: "int", nullable: false),
+                    StandardPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
